fabf06026a11905048568b3558a9081b8b28fbc5
VafaKhanim <vafakhanim.h@gmail.com>
2025-05-31 20:33:38 +0400

    changed

--- edgecut/settings.py
+++ edgecut/settings.py
@@ -48,7 +48,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = [os.getenv('CSRF_TRUSTED_ORIGINS')]
+CSRF_TRUSTED_ORIGINS = [
+    "51.21.197.203:8000/",
+]
+
 
 HOME_URL = os.getenv('HOME_URL')
 ABOUT_URL = os.getenv('ABOUT_URL')
